e5d017ee26bdb94f4c5d9ecfcdbc2ab768bd6c6c
Matthew Smith <matthew@matthewasmith.com>
2025-12-06 13:05:30 -0800

    castro config

--- main.py
+++ main.py
@@ -19,14 +19,18 @@ if on_raspberry_pi:
     epd = init_epd()
 
 # Example usage of the function
-STOP_ID_L_OWL_WESTBOUND = '16616'
-STOP_ID_L_OWL_EASTBOUND = '16617'
-STOP_ID_28_NORTHBOUND = '13394'
-STOP_ID_28_SOUTHBOUND = '13395'
-STOP_ID_CIVIC_CENTER_INBD = '15727'
-STOP_ID_CIVIC_CENTER_OTBD= '16997'
-STOP_ID_MARKET_8ST_INBD = '15651'
-STOP_ID_MARKET_8ST_OTBD = '15676'
+# STOP_ID_L_OWL_WESTBOUND = '16616'
+# STOP_ID_L_OWL_EASTBOUND = '16617'
+# STOP_ID_28_NORTHBOUND = '13394'
+# STOP_ID_28_SOUTHBOUND = '13395'
+# STOP_ID_CIVIC_CENTER_INBD = '15727'
+# STOP_ID_CIVIC_CENTER_OTBD= '16997'
+# STOP_ID_MARKET_8ST_INBD = '15651'
+# STOP_ID_MARKET_8ST_OTBD = '15676'
+STOP_ID_CASTRO_INDB = '15728'
+STOP_ID_CASTRO_OTBD = '16991'
+STOP_ID_CASTRO24_INDB = '14313'
+STOP_ID_CASTRO24_OTBD = '14334'
 
 
 def main():
@@ -43,10 +47,14 @@ def main():
     # Example: "June 15 — 3:45 PM"
     last_updated = f"{current_time} : {current_date}"
 
-    stop_data_inbound = get_muni_stop_data(STOP_ID_CIVIC_CENTER_INBD)
-    stop_data_outbound = get_muni_stop_data(STOP_ID_CIVIC_CENTER_OTBD)
-    stop_data_f_inbound = get_muni_stop_data(STOP_ID_MARKET_8ST_INBD)
-    stop_data_f_outbound = get_muni_stop_data(STOP_ID_MARKET_8ST_OTBD)
+    # stop_data_inbound = get_muni_stop_data(STOP_ID_CIVIC_CENTER_INBD)
+    # stop_data_outbound = get_muni_stop_data(STOP_ID_CIVIC_CENTER_OTBD)
+    # stop_data_f_inbound = get_muni_stop_data(STOP_ID_MARKET_8ST_INBD)
+    # stop_data_f_outbound = get_muni_stop_data(STOP_ID_MARKET_8ST_OTBD)
+    stop_data_inbound = get_muni_stop_data(STOP_ID_CASTRO_INDB)
+    stop_data_outbound = get_muni_stop_data(STOP_ID_CASTRO_OTBD)
+    stop_data_bus_in = get_muni_stop_data(STOP_ID_CASTRO24_INDB)
+    stop_data_bus_out = get_muni_stop_data(STOP_ID_CASTRO24_OTBD)
 
     # render muni stop
     formattedTimes = {
@@ -54,18 +62,26 @@ def main():
         # "times_L_em": get_formatted_arrival_times(get_muni_stop_data(STOP_ID_L_OWL_EASTBOUND), "L"),
         # "times_28_fw": get_formatted_arrival_times(get_muni_stop_data(STOP_ID_28_NORTHBOUND), "28"),
         # "times_28_dc": get_formatted_arrival_times(get_muni_stop_data(STOP_ID_28_SOUTHBOUND)),
-        "times_F_in": get_formatted_arrival_times(stop_data_f_inbound, "F"),
+        # "times_F_in": get_formatted_arrival_times(stop_data_f_inbound, "F"),
+        # "times_K_in": get_formatted_arrival_times(stop_data_inbound, "K"),
+        # "times_L_in": get_formatted_arrival_times(stop_data_inbound, "L"),
+        # "times_M_in": get_formatted_arrival_times(stop_data_inbound, "M"),
+        # "times_J_in": get_formatted_arrival_times(stop_data_inbound, "J"),
+        # "times_N_in": get_formatted_arrival_times(stop_data_inbound, "N"),
+        # "times_K_ot": get_formatted_arrival_times(stop_data_outbound, "K"),
+        # "times_F_ot": get_formatted_arrival_times(stop_data_f_outbound, "F"),
+        # "times_L_ot": get_formatted_arrival_times(stop_data_outbound, "L"),
+        # "times_M_ot": get_formatted_arrival_times(stop_data_outbound, "M"),
+        # "times_J_ot": get_formatted_arrival_times(stop_data_outbound, "J"),
+        # "times_N_ot": get_formatted_arrival_times(stop_data_outbound, "N"),
         "times_K_in": get_formatted_arrival_times(stop_data_inbound, "K"),
-        "times_L_in": get_formatted_arrival_times(stop_data_inbound, "L"),
-        "times_M_in": get_formatted_arrival_times(stop_data_inbound, "M"),
-        "times_J_in": get_formatted_arrival_times(stop_data_inbound, "J"),
-        "times_N_in": get_formatted_arrival_times(stop_data_inbound, "N"),
         "times_K_ot": get_formatted_arrival_times(stop_data_outbound, "K"),
-        "times_F_ot": get_formatted_arrival_times(stop_data_f_outbound, "F"),
+        "times_L_in": get_formatted_arrival_times(stop_data_inbound, "L"),
         "times_L_ot": get_formatted_arrival_times(stop_data_outbound, "L"),
+        "times_M_in": get_formatted_arrival_times(stop_data_inbound, "M"),
         "times_M_ot": get_formatted_arrival_times(stop_data_outbound, "M"),
-        "times_J_ot": get_formatted_arrival_times(stop_data_outbound, "J"),
-        "times_N_ot": get_formatted_arrival_times(stop_data_outbound, "N"),
+        "times_24_in": get_formatted_arrival_times(stop_data_bus_in, "24"),
+        "times_24_ot": get_formatted_arrival_times(stop_data_bus_out, "24"),
         "current_time": last_updated
     }
 
